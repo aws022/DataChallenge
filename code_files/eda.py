797b282a9ec8d59ee7810a4eeb7dfb0396ee8bbb
Alec Sanders <aws022@bucknell.edu>
2025-02-07 18:01:32 -0500

    Files added

--- code_files/eda.py
+++ code_files/eda.py
@@ -5,18 +5,23 @@ from traceback import print_tb
 import pandas as pd
 
 # Read in the file
-# Read the Tasks.md and answer all the questions using Python, Pandas, Numpy, Matplotlib, Seaboarn libraries.
-
 parent_dir = os.path.dirname(os.getcwd())
 data_dir = os.path.join(parent_dir, "data_files")
-compas_raw_file = os.path.join(data_dir, 'hot_100_current.csv')
+hot_100_raw_file = os.path.join(data_dir, 'hot_100_current.csv')
+love_songs_raw_file = os.path.join(data_dir, 'Love song categories for Billboard Top 10 hits, 1958 - September 2023, from The Pudding.csv')
+rolling_stone_raw_file = os.path.join(data_dir, 'rolling_stone.csv')
 
-if not os.path.exists(compas_raw_file):
-    print(f"File not found: {compas_raw_file}")
+
+if not os.path.exists(hot_100_raw_file):
+    print(f"File not found: {hot_100_raw_file}")
 else:
     print("File found, proceeding with reading.")
 
 
-df_test = pd.read_csv(compas_raw_file, nrows=5)  # Load first 5 rows without filtering
+df_test = pd.read_csv(hot_100_raw_file, nrows=5)  # Load first 5 rows without filtering
 
 print(df_test.columns)
+
+
+
+# Get artists with the most weeks on chart
