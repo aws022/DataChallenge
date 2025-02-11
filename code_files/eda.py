4a45b746d34ec96c6a33308008bf572eb2f2b021
Alec Sanders <aws022@bucknell.edu>
2025-02-11 17:53:59 -0500

    Age over time top 500

--- code_files/eda.py
+++ code_files/eda.py
@@ -82,12 +82,57 @@ print(rolling_stone.columns)
 
 
 '''
-Average age at top 500 album over time
+Average age at top 500 album over time ( by release year)
 '''
 
+# print(rolling_stone.columns)
+# group_age = rolling_stone.sort_values(by='Avg. Age at Top 500 Album', ascending=False)
+# name_age = group_age[['Clean Name', 'Avg. Age at Top 500 Album']]
+#
+#
+# print(name_age.head())
+#
+# bins = list(range(1900, 2030, 10))  # Decades from 1900 to 2020s
+# labels = [f"{decade}s" for decade in range(1900, 2020, 10)]
+#
+# decades = rolling_stone.groupby(pd.cut(rolling_stone['Release Year'], bins=bins, labels=labels))
+#
+# # Display first few rows from each group
+# print(decades.apply(lambda x: x.head()))
+#
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Define 5-year age bins from 15 to 55
+age_bins = list(range(15, 56, 5))  # 15-19, 20-24, ..., 50-54
+age_labels = [f"{i}-{i+4}" for i in range(15, 55, 5)]  # "15-19", "20-24", ..., "50-54"
+
+# Assign each artist to an age group
+rolling_stone['Age Group'] = pd.cut(rolling_stone['Avg. Age at Top 500 Album'], bins=age_bins, labels=age_labels, right=False)
+
+# Define decade bins for album release year
+bins = list(range(1950, 2030, 10))  # 1950s to 2020s
+labels = [f"{decade}s" for decade in range(1950, 2020, 10)]
+
+# Assign each album to a release decade
+rolling_stone['Decade'] = pd.cut(rolling_stone['Release Year'], bins=bins, labels=labels, right=False)
+
+# Count the number of artists per (Decade, Age Group)
+age_trend = rolling_stone.groupby(['Decade', 'Age Group']).size().unstack().fillna(0)
+
+# Plot the heatmap to show trends over time
+plt.figure(figsize=(12, 6))
+sns.heatmap(age_trend, annot=True, fmt=".0f", cmap="viridis", linewidths=0.5)
+
+# Formatting
+plt.xlabel("Age Group (Years)")
+plt.ylabel("Decade of Album Release")
+plt.title("Change in Artist Age Distribution Over Time (Top 500 Albums)")
+
+# Show plot
+plt.show()
+
 print(rolling_stone.columns)
-group_age = rolling_stone.sort_values(by='Avg. Age at Top 500 Album', ascending=False)
-name_age = group_age[['Clean Name', 'Avg. Age at Top 500 Album']]
 
-
-print(name_age.head())
