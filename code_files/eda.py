ca92ea2ee0d46c86298f18ad8a3160d134d2d4a5
Alec Sanders <aws022@bucknell.edu>
2025-02-11 17:40:40 -0500

    Ideas and average age top 500

--- code_files/eda.py
+++ code_files/eda.py
@@ -1,26 +1,38 @@
-print("Hello world")
 
 import os
 from traceback import print_tb
 import pandas as pd
 
-# Read in the file
+# File read in
 parent_dir = os.path.dirname(os.getcwd())
 data_dir = os.path.join(parent_dir, "data_files")
-hot_100_raw_file = os.path.join(data_dir, 'hot_100_current.csv')
-love_songs_raw_file = os.path.join(data_dir, 'Love song categories for Billboard Top 10 hits, 1958 - September 2023, from The Pudding.csv')
-rolling_stone_raw_file = os.path.join(data_dir, 'rolling_stone.csv')
+hot_100_path = os.path.join(data_dir, 'hot_100_current.csv')
+love_songs_path = os.path.join(data_dir, 'Love song categories for Billboard Top 10 hits, 1958 - September 2023, from The Pudding.csv')
+rolling_stone_path = os.path.join(data_dir, 'rolling_stone.csv')
 
 
-if not os.path.exists(hot_100_raw_file):
-    print(f"File not found: {hot_100_raw_file}")
+if not os.path.exists(hot_100_path):
+    print(f"File not found: {hot_100_path}")
+else:
+    print("File found, proceeding with reading.")
+
+if not os.path.exists(love_songs_path):
+    print(f"File not found: {love_songs_path}")
+else:
+    print("File found, proceeding with reading.")
+
+if not os.path.exists(rolling_stone_path):
+    print(f"File not found: {rolling_stone_path}")
 else:
     print("File found, proceeding with reading.")
 
 
-hot_100 = pd.read_csv(hot_100_raw_file)  # Load first 5 rows without filtering
-love_songs = pd.read_csv(love_songs_raw_file)
-rolling_stone = pd.read_csv(rolling_stone_raw_file)
+hot_100 = pd.read_csv(hot_100_path)
+love_songs = pd.read_csv(love_songs_path)
+rolling_stone = pd.read_csv(rolling_stone_path)
+
+
+
 
 ''' Ideas
 
@@ -36,4 +48,46 @@ rolling_stone = pd.read_csv(rolling_stone_raw_file)
     
     * Looking at 'Years Between Debut and Top 500 Album' in Rolling Stone 500
     and '2020-2003 Differential'
+    
+4. Changes in popularity of genres in these lists over time
+    THIS COLUMN IS INCOMPLETE AND DOES NOT GIVE USEFUL DATA
+    
+5. Average age at top 500 album over time
 '''
+
+
+# Group by genre
+'''
+# Separated by genre
+genre = rolling_stone.groupby('Album Genre', as_index=False).sum()
+
+#Show 2020 - 2003 differential by genre
+diff_genre = genre.sort_values(by='2020-2003 Differential', ascending=True)
+
+print(diff_genre)
+
+'''
+
+
+'''
+# How many love songs in the top 100 each decade?
+
+spotify_most_popular = rolling_stone.groupby('Spotify Popularity', as_index=False).sum()
+ordered = spotify_most_popular.sort_values(by='Spotify Popularity', ascending=False)
+
+print(ordered)
+
+print(rolling_stone.columns)
+'''
+
+
+'''
+Average age at top 500 album over time
+'''
+
+print(rolling_stone.columns)
+group_age = rolling_stone.sort_values(by='Avg. Age at Top 500 Album', ascending=False)
+name_age = group_age[['Clean Name', 'Avg. Age at Top 500 Album']]
+
+
+print(name_age.head())
